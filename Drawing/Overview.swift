765c287610c61148502dbd094285b4cb546bb21e
Edmond Podlegaev <edpodlegaev@yandex.ru>
2023-04-25 21:49:56 +0300

    Paths vs shapes in SwiftUI

--- Drawing/Overview.swift
+++ Drawing/Overview.swift
@@ -7,6 +7,36 @@
 
 import SwiftUI
 
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        
+        return path
+    }
+}
+
+struct Arc: Shape {
+    let startAngle: Angle
+    let endAngle: Angle
+    let clockwise: Bool
+    
+    func path(in rect: CGRect) -> Path {
+        let rotationAdjustment = Angle.degrees(90)
+        let modifierStart = startAngle - rotationAdjustment
+        let modifiedEnd = endAngle - rotationAdjustment
+        var path = Path()
+        
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: modifierStart, endAngle: modifiedEnd, clockwise: !clockwise)
+        
+        return path
+    }
+}
+
 struct Overview: View {
     var body: some View {
         Path { path in
@@ -17,6 +47,13 @@ struct Overview: View {
             path.closeSubpath()
         }
         .stroke(.blue, style:  StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+        
+        Triangle()
+            .stroke(.red, style:  StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+            .frame(width: 300, height: 300)
+        Arc(startAngle: .degrees(0), endAngle: .degrees(110), clockwise: true)
+            .stroke(.blue, lineWidth: 10)
+            .frame(width: 300, height: 300)
     }
 }
 
