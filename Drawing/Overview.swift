7c36062c3f84641ea6d5d503bfbe25c53a5d7865
Edmond Podlegaev <edpodlegaev@yandex.ru>
2023-04-25 21:10:17 +0300

    Add Overview

---
+++ Drawing/Overview.swift
@@ -0,0 +1,20 @@
+//
+//  Overview.swift
+//  Drawing
+//
+//  Created by Edmond Podlegaev on 25.04.2023.
+//
+
+import SwiftUI
+
+struct Overview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Overview_Previews: PreviewProvider {
+    static var previews: some View {
+        Overview()
+    }
+}
